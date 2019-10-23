955574d8329b7f7d0d4ffd66608d5e70c8deaef6
B404Plarrison <rateebriyasat@gmail.com>
2019-10-22 21:02:42 -0400

    imported all website files

---
+++ backend/index.js
@@ -0,0 +1,72 @@
+const express =  require('express')
+const app = express()
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+const gen = require('word-concat-js')
+
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use(express.static('../'))
+
+async function keygen(){
+    var string = gen(3, 4, 10, "_")
+    console.log(string)
+    const getString = `SELECT * FROM active`
+    /*
+    fix this BRUH
+    await getConnection().query(getString, async (err, rows, fields)=>{
+        if(rows!=undefined){
+            return await keygen()
+        }else{
+            console.log("uh")
+            console.log(string, "uh")
+            return await string
+
+        }
+    })
+    */
+   
+  
+    return string;
+}
+
+function getConnection(){
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'radardomcgee135',
+        database: 'requests',
+        insecureAuth: true,
+    })
+}
+
+app.get("/",(req,res)=>{
+    console.log("started")
+})
+
+app.post('/requestsent',async (req, res)=>{
+    console.log('creating request...')
+    const requesta = req.body.request
+    var keyd = await keygen()
+    console.log(keyd, "key")
+    const queryString = "INSERT INTO active (char, reqtext) SET (?,?)"
+    getConnection().query(queryString, [keyd, requesta],(err, results, fields)=>{
+        if(err){
+            console.log(err.message)
+        }
+        console.log("Inserted request with", requesta, keyd)
+        console.log(results)
+        console.log(fields)
+        res.end()
+    })
+    const getString = `SELECT * FROM active`
+    getConnection().query(getString, async (err, rows, fields)=>{
+        await console.log(rows)   
+   })
+    //res.end()
+})
+
+app.listen(3003, ()=>{
+    console.log("server is up and listening on 3003...")
+})
